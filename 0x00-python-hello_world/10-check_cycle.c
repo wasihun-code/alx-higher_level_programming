ebb0830b20b069055f344704e76eb44d58495883
wasihun-code <wasihunageru7947@gmail.com>
2021-09-06 22:54:45 +0300

    ante junta

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
+/**
+ *check_cycle - checks if there is a cycle in a linked list.
+ *@list: head of the linked least
+ *Return: 1 if there is a loop, 0-no loop
+ */
+
 int check_cycle(listint_t *list)
 {
-  return (0);
+	listint_t *fast_pointer, *slow_pointer;
+
+	if (list == NULL)
+		return (0);
+
+	fast_pointer = list;
+	slow_pointer = list;
+	
+	while (fast_pointer && fast_pointer->next)
+	{
+		list = list->next;
+		slow_pointer = slow_pointer->next;
+		fast_pointer = fast_pointer->next->next;
+		if (fast_pointer == slow_pointer)
+			return (1);
+	}
+	
+	return (0);
 }
