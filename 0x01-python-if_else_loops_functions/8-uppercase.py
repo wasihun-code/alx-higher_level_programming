c321fa4161313c3d9203a8f51e303abba27fb078
wasihun-code <wasihunageru7947@gmail.com>
2021-09-07 20:58:43 +0300

    One of the best Requirement failure o all && Nardos Berihun

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     str2 = ''
     for i in str:
         if ord(i) >= 97 and ord(i) <= 123:
-            str2+=chr(ord(i)-32)
+            str2 += chr(ord(i)-32)
         else:
-            str2+=i
+            str2 += i
     str = str2
     print("{}".format(str))
