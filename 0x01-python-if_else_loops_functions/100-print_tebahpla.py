0c15c282ce72367741f304870e7eec5aab13a18b
wasihun-code <wasihunageru7947@gmail.com>
2021-09-07 22:24:59 +0300

    Requirements

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(122, 96, -1):
-	if num % 2 == 1:
-		ascii_num = num - 32
-	print("{:c}".format(num), end='')
+    if num % 2 == 1:
+        num = num - 32
+    print("{:c}".format(num), end='')
