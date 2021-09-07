9d68b59dda9f1d58b435e4478fc2c506ad42f92d
wasihun-code <wasihunageru7947@gmail.com>
2021-09-07 20:50:55 +0300

    now to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str2 = ''
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 123:
+            str2+=chr(ord(i)-32)
+        else:
+            str2+=i
+    print(str2)
