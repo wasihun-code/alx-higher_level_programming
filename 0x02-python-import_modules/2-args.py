cb0d05a52d5e05d4e0cf46c5ba6da0710e890997
wasihun-code <wasihunageru7947@gmail.com>
2021-09-09 14:10:33 +0300

    betty has gone vacation and Now the king is PEP8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 import sys
 l = len(sys.argv)
+
+
 def printArguments():
+    """Print command line arguments
+
+    Args:
+        command line arguments
+
+    Return:
+        0 since void
+    """
+
     if (l == 2):
         print("{} argument:".format(l - 1))
         print("{}: {}".format(1, sys.argv[1]))
+
     else:
         print("{} arguments:".format(l - 1))
         for i in range(1, l):
             print("{}: {}".format(i, sys.argv[i]))
+
 if __name__ == "__main__":
     printArguments()
