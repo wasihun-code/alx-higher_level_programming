09a32c6b060f67c69419e2a12f6da4c94cd406da
wasihun-code <wasihunageru7947@gmail.com>
2021-09-09 14:00:59 +0300

    just thinking right

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 l = len(sys.argv)
-if (l == 1):
-    print("0 arguments.")
-elif (l == 2):
-    print("1 argument.")
-    print("{}: {}".format(1, sys.argv[1]))
-else:
-    print("{} arguments.".format(l - 1))
-    for i in range(1, l):
-        print("{}: {}".format(i, sys.argv[i]))
+def printArguments():
+    if (l == 2):
+        print("1 argument.")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments.".format(l - 1))
+        for i in range(1, l):
+            print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    printArguments()
