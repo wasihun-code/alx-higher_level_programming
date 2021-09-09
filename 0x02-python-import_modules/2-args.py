9e7ca32840e55dcc7acc6fa5e801bd5092b1575a
wasihun-code <wasihunageru7947@gmail.com>
2021-09-09 14:05:19 +0300

    Hope it works now

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 l = len(sys.argv)
 def printArguments():
     if (l == 2):
-        print("1 argument.")
+        print("{} argument:".format(l - 1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments.".format(l - 1))
+        print("{} arguments:".format(l - 1))
         for i in range(1, l):
             print("{}: {}".format(i, sys.argv[i]))
 if __name__ == "__main__":
