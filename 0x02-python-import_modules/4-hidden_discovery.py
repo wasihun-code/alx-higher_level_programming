3116e28188c66b4945ef97a511332c25a3e0d619
wasihun-code <wasihunageru7947@gmail.com>
2021-09-10 11:41:04 +0300

    easy_rint

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Mods in hidden 4"""
-    import hidden_4 as hidden
-
-    for name in dir(hidden):
-        if name[0:2] != "__":
-            print(name)
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
