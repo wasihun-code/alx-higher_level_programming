057739145b82510a8dbd3d598adcb8f97f680da1
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 01:31:21 +0300

    My calculator is up and running

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
+if __name__ == "__main__":
 
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), result))
+    if (len(sys.argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
+    num1, op, num2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
+    if op == '+':
+        res = add(num1, num2)
+    elif op == '-':
+        res = sub(num1, num2)
+    elif op == '/':
+        res = div(num1, num2)
+    elif op == '*':
+        res = mul(num1, num2)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(num1, op, num2, res))
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+
+for i in range(0, len(sys.argv)):
+    print(i, sys.argv[0])
+print(len(sys.argv))
