8321737e4c56a5f7a9c8229f1465ca3ade58e98f
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 13:16:34 +0300

    is it palindrom or not

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,17 +1,13 @@
-#!/bin/python3
+#!/usr/bin/python3
+# 4-new_in_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
-    l = len(my_list)
-    new_list = []
-
-    if l < 0:
-        return None
-
-    if idx < 0 or idx > l - 1:
-        return my_list.copy()
-
-    new_list = my_list.copy()
-    new_list[idx] = element
-
-    return new_list
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
