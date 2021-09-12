906cda9d21f7ce3e4290167a00f8ea5403a3184e
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 12:44:02 +0300

    my_list.copy()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,9 +9,9 @@ def new_in_list(my_list, idx, element):
         return None
 
     if idx < 0 or idx > l - 1:
-        return my_list
+        return my_list.copy()
 
-    new_list = my_list[:]
+    new_list = my_list.copy()
     new_list[idx] = element
 
     return new_list
