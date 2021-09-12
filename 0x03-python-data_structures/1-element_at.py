0b43f8e9027c2359f4a562410c82a93f1b5013d8
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 12:18:03 +0300

    idx is from 0 not 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@
 
 def element_at(my_list, idx):
 
-    if len(my_list) <= 0 or idx < 0 or idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    if len(my_list) <= 0 or idx < 0 or idx > len(my_list) - 1:
+        return
+    return my_list[idx]
