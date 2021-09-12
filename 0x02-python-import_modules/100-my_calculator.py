4c7d7d5508d14871818cd717e784cb0700525802
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 13:37:16 +0300

    Dictionaries in python are awesome right?

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,16 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) != 4):
+    if (len(sys.argv) - 1 != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
-    num1, op, num2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+    num1, operator, num2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+    op = {"+": add, "-": sub, "*": mul, "/": div}
 
-    if op == '+':
-        res = add(num1, num2)
-    elif op == '-':
-        res = sub(num1, num2)
-    elif op == '/':
-        res = div(num1, num2)
-    elif op == '*':
-        res = mul(num1, num2)
-    else:
+    if sys.argv[2] not in list(op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
-    print("{:d} {:d} {} = {:d}".format(num1, op, num2, res))
+    print("{} {} {} = {}"
+        .format(num1, sys.argv[2], num2, op[sys.argv[2]](num1, num2)))
