987b681ea673521b892cb4a1c5531a375b990d99
wasihun-code <wasihunageru7947@gmail.com>
2021-09-12 13:43:05 +0300

    Dictionaries in python are awesome right?

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) - 1 != 3):
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    num1, operator, num2 = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
     op = {"+": add, "-": sub, "*": mul, "/": div}
-
     if sys.argv[2] not in list(op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}"
-        .format(num1, sys.argv[2], num2, op[sys.argv[2]](num1, num2)))
+    num1, num2, c = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
+
+    print("{} {} {} = {}".format(num1, c, num2, op[c](num1, num2)))
