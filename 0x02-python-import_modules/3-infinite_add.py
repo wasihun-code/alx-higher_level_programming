5534c18cc2bfa0b840c7cd49a95ea862d63f45ba
wasihun-code <wasihunageru7947@gmail.com>
2021-09-16 15:18:33 +0300

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-        import sys
+    import sys
 
-        n = len(sys.argv)
-        total = 0
+    n = len(sys.argv)
+    total = 0
 
-        for i in range(n-1):
-            total += int(sys.argv[i + 1])
+    for i in range(n-1):
+        total += int(sys.argv[i + 1])
 
-        print(total)
+    print(total)
