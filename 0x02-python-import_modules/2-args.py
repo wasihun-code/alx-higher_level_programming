7588d04ffdc2c29873350d7358c9499e000734d2
wasihun-code <wasihunageru7947@gmail.com>
2021-09-16 15:15:48 +0300

    args vs python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,16 +4,18 @@ import sys
 
 def printArguments():
 
-    if (len(sys.argv) == 1):
+    length = len(sys.argv)
+    if (length == 1):
         print("0 arguments.")
-    elif (len(sys.argv) == 2):
-        print("{} argument:".format(len(sys.argv) - 1))
+    elif (length == 2):
+        print("{} argument:".format(length - 1))
         print("{}: {}".format(1, sys.argv[1]))
 
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
+        print("{} arguments:".format(length - 1))
+        for i in range(1, length):
             print("{}: {}".format(i, sys.argv[i]))
+
 
 if __name__ == "__main__":
     printArguments()
