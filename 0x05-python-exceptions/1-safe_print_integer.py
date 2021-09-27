d736da0d1900c081d5ee0af484e97e190b6ce13c
wasihun-code <wasihunageru7947@gmail.com>
2021-09-27 17:11:58 +0530

    Second exception handler

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,8 @@
 
 def safe_print_integer(value):
     try:
-        value = int(value)
         print("{:d}".format(value))
         return True
     except ValueError:
+        pass
         return False
