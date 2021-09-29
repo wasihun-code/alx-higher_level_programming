9e1d852c1da82025a1cff8f6cdde50e431792011
wasihun-code <wasihunageru7947@gmail.com>
2021-09-29 21:04:26 +0530

    Private instance size Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square and a private instance Size"""
+
+
+class Square:
+    """Represent a square with size as private"""
+
+    def __init__(self, size):
+        self.__size = size
