4baf06ee92b57d7ffae040b8c1de8cefd5d03ba8
wasihun-code <wasihunageru7947@gmail.com>
2021-10-04 23:28:37 +0530

    second: Tasks 1, 2 and 3

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,6 +24,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
