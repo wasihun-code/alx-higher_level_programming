12f50fb21038c7c822f2b73fee0796cbeff009c8
wasihun-code <wasihunageru7947@gmail.com>
2021-10-12 19:23:40 +0530

    what

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Returns the string representation of a file in json."""
+
+
+def to_json_string(my_obj):
+    """And this is the function meeeeeeeeeeeeeeeen
+
+    Args:
+        my_obj (str): string to be represented
+
+    """
+
+    import json
+    return json.dumps(my_obj)
