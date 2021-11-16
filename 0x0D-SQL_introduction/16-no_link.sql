479f433482128f3a6de8a882dedd2b97c502fb94
wasihun-code <wasihunageru7947@gmail.com>
2021-11-17 00:06:10 +0530

    task last or 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists all recoreds of second table hbtn_0c_0 database
 -- records ordered by score top first
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/17-tempinsert.sql
@@ -0,0 +1,5 @@
+-- inserts a new row in the first_table in hbtn_0c_0 database
+INSERT INTO second_table (id, name, score)
+VALUES(6, NULL, 18);
+INSERT INTO second_table
+VALUES(7, NULL, 12);
